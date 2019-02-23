92c1bdbbcc10f9adb95c0d3ae363373e4f79867a
mattbat <mrmacman@gmail.com>
2019-02-23 07:49:30 -0600

    WIP Update HA custom component
    I struggled to import the VeSync classes properly, so I copied the library to the custom compoent's folder and imported from the current directory (".")  This is a WIP and shouldn't be left this way.
    
    A new class (VeSyncWallSwitchHA) was created to represent the wall switch since it doesn't supprort voltage and power information.

--- HACustom_Components/switch/vesync.py
+++ HACustom_Components/switch/vesync.py
@@ -1,6 +1,5 @@
 """
 Support for Etekcity VeSync switches.
-
 For more details about this platform, please refer to the documentation at
 https://home-assistant.io/components/switch.vesync/
 """
@@ -23,7 +22,10 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
 
 def setup_platform(hass, config, add_entities, discovery_info=None):
     """Set up the VeSync switch platform."""
-    from custom_components.pyvesync-v2 import VeSync
+    #from custom_components.pyvesync-v2 import VeSync
+    from . vesync import VeSync
+    from . vesync import VeSyncSwitch
+    from . vesync import VeSyncWallSwitch   
 
     switches = []
 
@@ -45,16 +47,23 @@ def setup_platform(hass, config, add_entities, discovery_info=None):
                      len(manager.devices), count_string)
 
         for switch in manager.devices:
-            switches.append(VeSyncSwitchHA(switch))
-            _LOGGER.info("Added a VeSync switch named '%s'",
-                         switch.device_name)
+            if type(switch) is VeSyncSwitch:
+                switches.append(VeSyncSwitchHA(switch))
+                _LOGGER.info("Added a VeSync switch named '%s'",
+                            switch.device_name)
+            elif type(switch) is VeSyncWallSwitch:
+                 switches.append(VeSyncWallSwitchHA(switch))
+                 _LOGGER.info("Added a VeSync wall switch named '%s'",
+                            switch.device_name)
     else:
         _LOGGER.info("No VeSync devices found")
 
     add_entities(switches)
 
 
+
 class VeSyncSwitchHA(SwitchDevice):
+    
     """Representation of a VeSync switch."""
 
     def __init__(self, plug):
@@ -106,3 +115,44 @@ class VeSyncSwitchHA(SwitchDevice):
         self.smartplug.update()
         self._current_power_w = self.smartplug.get_power()
         self._today_energy_kwh = self.smartplug.get_kwh_today()
+
+        
+class VeSyncWallSwitchHA(SwitchDevice):
+    
+    """Representation of a VeSync wall switch."""
+
+    def __init__(self, plug):
+        """Initialize the VeSync switch device."""
+        self.smartplug = plug
+
+    @property
+    def unique_id(self):
+        """Return the ID of this switch."""
+        return self.smartplug.uuid
+
+    @property
+    def name(self):
+        """Return the name of the switch."""
+        return self.smartplug.device_name
+
+    @property
+    def available(self) -> bool:
+        """Return True if switch is available."""
+        return self.smartplug.connection_status == "online"
+
+    @property
+    def is_on(self):
+        """Return True if switch is on."""
+        return self.smartplug.device_status == "on"
+
+    def turn_on(self, **kwargs):
+        """Turn the switch on."""
+        self.smartplug.turn_on()
+
+    def turn_off(self, **kwargs):
+        """Turn the switch off."""
+        self.smartplug.turn_off()
+
+    def update(self):
+        """Handle data changes for node values."""
+        self.smartplug.update()
